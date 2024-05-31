0e38c5f71623e2e14bb055b092310b8b988171aa
Eric Ouellette <ericfrontdev@outlook.com>
2024-05-31 18:53:37 -0400

    imageRoutes

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -32,6 +32,8 @@ const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require('./routes/searchRoutes');
 
+const imageRoutes = require('./routes/imageRoutes');
+
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -47,6 +49,8 @@ app.use('/api/secondchance/items', secondChanceItemsRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/secondchance/search', searchRoutes);
+
+app.use('/images', imageRoutes);
 
 
 // Global Error Handler
