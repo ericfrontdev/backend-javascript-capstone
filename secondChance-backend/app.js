c202416077acb2f61973f9503c0bc17f5681cc31
Eric Ouellette <ericfrontdev>
2024-05-31 05:48:41 -0400

    dification to routes path

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -24,13 +24,13 @@ app.use(express.json());
 // Route files
 
 // authRoutes Step 2: import the authRoutes and store in a constant called authRoutes
-const authRoutes = require('./authRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 // Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
 const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-const searchRoutes = require('./searchRoutes');
+const searchRoutes = require('./routes/searchRoutes');
 
 
 const pinoHttp = require('pino-http');
